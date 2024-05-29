321b9f45954b5bdf4b28e49a2bfc674ff30761aa
iamBot <iamBot@gmail.com>
2024-05-29 10:39:47 +0530

    t

--- src/client/Header.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Heading() { return <h1>Keeper</h1> };
-
-export {Heading};
-
--- src/client/footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Foot() 
-{
-    return <p>Copyright ⓒ {new Date().getFullYear()}</p>;
-}
-
-export {Foot};
